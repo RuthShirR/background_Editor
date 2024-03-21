6e6a05857461ad09470e2d38fd951d84afc96eff
shir <ruthshir31@gmail.com>
2024-03-21 17:33:47 +0200

    download & tabs

--- src/Components/Bg/Bg.tsx
+++ src/Components/Bg/Bg.tsx
@@ -1,20 +1,55 @@
 import "./Bg.css";
-import close from "../../Assets/Images/close.png"
+import close from "../../Assets/Images/close.png";
+import Download from "../Download/Download";
+import cleanup_icon from "../../Assets/Images/Cleanup Edges.png";
+import image_icon from "../../Assets/Images/Image.png";
 
 function Bg(): JSX.Element {
-    return (
-      <div className="Bg">
-        <div className="background_cont">
-          <div className="header_title">
-            {" "}
-            העלאת תמונה על מנת להסיר לה את הרקע{" "}
+  return (
+    <div className="Bg">
+      <div className="background_cont">
+        <div className="header_title">
+          {" "}
+          העלאת תמונה על מנת להסיר לה את הרקע{" "}
+        </div>
+        <img src={close} alt="close" className="close_top" />
+        <div className="upload_btn"> העלאת תמונה </div>
+        <div className="content_container">
+          <div className="main_content">
+            <div className="main_top">
+              <div className="tabs_txt no_bg">
+                הסר רקע
+                <img className="tab_icon" src={cleanup_icon} alt="cleanup" />
+              </div>
+              <div className="tabs_txt">
+                מקורי
+                <img className="tab_icon" src={image_icon} alt="image_i" />
+              </div>
+            </div>
+            <div className="main_in_content"></div>
           </div>
-          <img src={close} alt="close" className="close_top" />
-          <div className="upload_btn"> העלאת תמונה </div>
-          <div className="content_container"></div>
+          <div className="side_content">
+            <div className="side_in_content">
+              <Download
+                title="תמונה חינם"
+                description="612x408 תצוגה מקדימה של תמונה "
+                button="הורד"
+                small_txt=" איכות טובה עד 0.25 מגה פיקסל "
+                position="top"
+              ></Download>
+              <Download
+                title="Pro"
+                description="1280x1920 תצוגה מלאה "
+                button="HD הורד"
+                small_txt=" איכות מירבית עד 25 מגה פיקסל "
+                position="bottom"
+              ></Download>
+            </div>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Bg;
---
+++ src/Components/Download/Download.tsx
@@ -0,0 +1,37 @@
+import "./Download.css";
+import new_icon from "../../Assets/Images/new.png";
+import check from "../../Assets/Images/check.png"
+
+interface DownloadProps {
+  title: string;
+  description: string;
+  button: string;
+  small_txt: string;
+  position: string;
+}
+function Download(props: DownloadProps): JSX.Element {
+  return (
+    <div
+      className={
+        "Download" + (props.position === "bottom" ? " border_top" : "")
+      }
+    >
+      <div className="download_title">
+        {props.title}
+        {props.position === "bottom" ? (
+          <img className="new_icon" src={new_icon} alt="new" />
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="download_description">{props.description}</div>
+      <button className="download_btn">{props.button}</button>
+      <div className="small_txt">{props.small_txt}
+      <img src={check} alt="check" className="check_icon" />
+      </div>
+
+    </div>
+  );
+}
+
+export default Download;
